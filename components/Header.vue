fd2477c1f504a2e78555c1a41038c853e85d5707
Peter Barrow <pbarrowbyu@gmail.com>
2021-03-12 08:54:28 -0700

    portfolio pic

--- components/Header.vue
+++ components/Header.vue
@@ -1,13 +1,16 @@
 <template>
-  <div id="header" class="flex justify-between bg-blue-200 min-w-full">
-    <div class="p-8">
-      <NuxtLink to="/" class="text-2xl font-bold text-gray-900"
-        >Peter Barrow</NuxtLink
-      >
-    </div>
-    <div class="flex justify-between p-8">
-      <!-- <NuxtLink class="p-2" to="/projects">Projects</NuxtLink> -->
-      <p class="p-2">About</p>
+  <div>
+    <div class="header_border"></div>
+    <div id="header" class="flex justify-between bg-blue-200 min-w-full">
+      <div class="flex justify-between p-3">
+        <img class="profile_pic" src="~/assets/images/profile_square.png" />
+        <NuxtLink to="/" class="pl-10 pt-11 text-2xl font-bold text-gray-900"
+          >Peter Barrow</NuxtLink
+        >
+      </div>
+      <div class="flex justify-between p-3">
+        <NuxtLink class="p-12" to="/about"><p>About</p></NuxtLink>
+      </div>
     </div>
   </div>
 </template>
@@ -21,5 +24,15 @@ export default {
 <style>
 #header {
   font-family: 'Merriweather';
+}
+.header_border {
+  width: 100%;
+  height: 0.5rem;
+  background-color: rgb(83, 83, 253);
+}
+.profile_pic {
+  border-radius: 50%;
+  /* border: 5px; */
+  margin-bottom: 1rem;
 }
 </style>
---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- <div class="container"> -->
+  <div>About Page here</div>
+  <!-- </div> -->
+</template>
+
+<script></script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Merriweather:wght@300&display=swap');
+</style>
