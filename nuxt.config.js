2114a4a94743790663140cb9d0523efd08c1c609
Peter Barrow <pbarrowbyu@gmail.com>
2021-03-12 09:45:53 -0700

    added about page

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'portfolio',
+    title: 'Peter Barrow portfolio',
     htmlAttrs: {
       lang: 'en',
     },
--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,18 @@
 <template>
-  <!-- <div class="container"> -->
-  <div>About Page here</div>
-  <!-- </div> -->
+  <div class="w-screen">
+    <div class="justify-items-center grid m-20">
+      <h1 class="justify-self-auto font-bold p-6">About Peter</h1>
+      <div>
+        <p class="pt-6">
+          I recently decided to pursue a career in web development, pivoting
+          from my prior career in actuarial analysis. The problem solving and
+          model building aspect of actuarial work enticed me to make the switch.
+          I’ve been teaching myself through various online resources, primarily
+          FreeCodeCamp.org and Frontend Masters.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script></script>
