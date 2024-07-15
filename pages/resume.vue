a7deacd47118915612405e0fd136b5f91531bbbc
Peter Barrow <peter.august.barrow@gmail.com>
2024-07-15 15:23:13 -0600

    Updated skills

--- pages/resume.vue
+++ pages/resume.vue
@@ -163,8 +163,9 @@
             apply technical skills in practical projects.
           </p>
           <p class="mt-4 text-base leading-8 text-gray-500 lg:text-xl">
-            Technical skills: HTML/HTML5, CSS, Javascript/JS, Vue/Vuex, Quasar,
-            React/Redux, TypeScript, Tailwind, Bootstrap, Chrome dev tools
+            Technical skills: HTML/HTML5, CSS, Javascript/JS, React, React
+            Native, Remix, Redux, Vue/Vuex, Quasar, TypeScript, Tailwind,
+            Bootstrap, Chrome dev tools
           </p>
           <hr class="mx-auto mt-2 rounded" />
         </div>
